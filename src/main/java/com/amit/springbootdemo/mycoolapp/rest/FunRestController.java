ff13bdaab5661f6cdc0283590aae7608fb7bbfe7
Amit Sunil Mishra <amitmishra-110>
2024-01-29 23:03:13 +0530

    Initial commit for testing spring boot app with intializr

---
+++ src/main/java/com/amit/springbootdemo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.amit.springbootdemo.mycoolapp.rest;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    //Expose  / endpoint that returns a hello world !!
+
+    @GetMapping("/")
+    public String  sayHelloWorld()
+    {
+        return "Hello World!!!";
+    }
+}
